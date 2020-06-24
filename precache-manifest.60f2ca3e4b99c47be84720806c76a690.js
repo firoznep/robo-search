b6e57d1a5574031b91fdd8b918b80eb62cdc5204
firoznep <firoz.thekhan@gmail.com>
2020-06-24 15:42:13 +0545

    Updates

---
+++ precache-manifest.60f2ca3e4b99c47be84720806c76a690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83160efef205e6b51c38cbd9b1af411d",
+    "url": "/robo-search/index.html"
+  },
+  {
+    "revision": "5d8d8fac2f47685d8b1e",
+    "url": "/robo-search/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cad06d9909a14a1013db",
+    "url": "/robo-search/static/css/main.db857b37.chunk.css"
+  },
+  {
+    "revision": "5d8d8fac2f47685d8b1e",
+    "url": "/robo-search/static/js/2.03d82dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-search/static/js/2.03d82dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad06d9909a14a1013db",
+    "url": "/robo-search/static/js/main.f390c364.chunk.js"
+  },
+  {
+    "revision": "b566477f3b3bf9c4461e",
+    "url": "/robo-search/static/js/runtime-main.1affa3ba.js"
+  }
+]);
